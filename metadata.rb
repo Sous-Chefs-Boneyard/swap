3be3df0867481c194a9d6b3723376f3060c098cb
Andrew Gross <andrew@yipit.com>
2013-02-28 10:36:46 -0500

    Tweaks for formatting

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,8 @@
+name "swap"
+
 maintainer 'Seth Vargo'
 maintainer_email 'sethvargo@gmail.com'
 license 'Apache 2.0'
 description 'Provides an LWRP for easily creating a managing swap files and swap partitions.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.1.1'
-
-name "swap"
