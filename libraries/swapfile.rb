9242183443042dcc5fa4ae456adeb4df48725f3c
Andrew Gross <andrew@yipit.com>
2013-04-09 14:14:44 -0400

    Switch from _ convention to protected methods

--- libraries/swapfile.rb
+++ libraries/swapfile.rb
@@ -1,69 +1,63 @@
 module Swapfile
   module Helpers
 
-    # The block size (1MB)
-    def block_size
-      1048576
-    end
-
-    def swap_exists?(nr)
-      command = "grep #{nr.path} /proc/swaps"
-      result = %x[#{command}]
-      does_swap_exist = ! result.empty?
-      Chef::Log.debug("#{nr} Swap Exists? #{does_swap_exist}")
-      does_swap_exist
-    end
-
     def swap_creation_command(nr)
       if compatible_filesystem?(nr) && compatible_kernel(nr)
-        command = _get_fallocate_command(nr)
+        command = get_fallocate_command(nr)
       else
-        command = _get_dd_command(nr)
+        command = get_dd_command(nr)
       end
       Chef::Log.debug("#{nr} swap creation command is '#{command}'")
       command
     end
 
     def fallback_swap_creation_command(nr)
-      command = _get_dd_command(nr)
+      command = get_dd_command(nr)
       Chef::Log.debug("#{nr} fallback swap creation command is '#{command}'")
       command
     end
 
-    def _get_fallocate_size(nr)
-      size = block_size * nr.size
-      Chef::Log.debug("#{nr} fallocate size is #{size}")
-      size
-    end
+    protected
 
-    def _get_fallocate_command(nr)
-      size = _get_fallocate_size(nr)
-      command = "fallocate -l #{size} #{nr.path}"
-      Chef::Log.debug("#{nr} fallocate command is '#{command}'")
-      command
-    end
+      # The block size (1MB)
+      def block_size
+        1048576
+      end
 
-    def _get_dd_command(nr)
-      command = "dd if=/dev/zero of=#{nr.path} bs=#{block_size} count=#{nr.size}"
-      Chef::Log.debug("#{nr} dd command is '#{command}'")
-      command
-    end
+      def get_fallocate_size(nr)
+        size = block_size * nr.size
+        Chef::Log.debug("#{nr} fallocate size is #{size}")
+        size
+      end
 
-    def compatible_kernel(nr)
-      fallocate_location = %x[which fallocate]
-      Chef::Log.debug("#{nr} fallocate location is '#{fallocate_location}'")
-      ::File.exists?(fallocate_location.chomp)
-    end
+      def get_fallocate_command(nr)
+        size = get_fallocate_size(nr)
+        command = "fallocate -l #{size} #{nr.path}"
+        Chef::Log.debug("#{nr} fallocate command is '#{command}'")
+        command
+      end
 
-    def compatible_filesystem?(nr)
-      compatible_filesystems = ['xfs', 'ext4']
-      parent_directory = ::File.dirname(nr.path)
-      # Get FS info, get second line as first is column headings
-      command = "df -T #{parent_directory} | awk 'NR==2 {print $2}'"
-      result = %x[#{command}]
-      Chef::Log.debug("#{nr} filesystem listing is '#{result}'")
-      compatible_filesystems.any? { |fs| result.include? fs }
-    end
+      def get_dd_command(nr)
+        command = "dd if=/dev/zero of=#{nr.path} bs=#{block_size} count=#{nr.size}"
+        Chef::Log.debug("#{nr} dd command is '#{command}'")
+        command
+      end
+
+      def compatible_kernel(nr)
+        fallocate_location = %x[which fallocate]
+        Chef::Log.debug("#{nr} fallocate location is '#{fallocate_location}'")
+        ::File.exists?(fallocate_location.chomp)
+      end
+
+      def compatible_filesystem?(nr)
+        compatible_filesystems = ['xfs', 'ext4']
+        parent_directory = ::File.dirname(nr.path)
+        # Get FS info, get second line as first is column headings
+        command = "df -T #{parent_directory} | awk 'NR==2 {print $2}'"
+        result = %x[#{command}]
+        Chef::Log.debug("#{nr} filesystem listing is '#{result}'")
+        compatible_filesystems.any? { |fs| result.include? fs }
+      end
 
   end
 end
