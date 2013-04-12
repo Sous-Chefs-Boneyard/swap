5f339039c1325792018300b32e22addfbd0f5648
Andrew Gross <andrew@yipit.com>
2013-04-12 13:45:08 -0400

    Fix method signatures

--- libraries/swapfile_provider.rb
+++ libraries/swapfile_provider.rb
@@ -14,8 +14,8 @@ class Chef
       end
 
       def action_create
-        command = swap_creation_command(@new_resource)
-        fallback_command = fallback_swap_creation_command(@new_resource)
+        command = swap_creation_command
+        fallback_command = fallback_swap_creation_command
         if swap_enabled?
           Chef::Log.debug("#{@new_resource} already created - nothing to do")
         else
@@ -84,19 +84,19 @@ class Chef
           ! swapfile_regex.match(enabled_swapfiles).nil?
         end
 
-        def swap_creation_command(nr)
-          if compatible_filesystem?(nr) && compatible_kernel(nr)
-            command = get_fallocate_command(nr)
+        def swap_creation_command
+          if compatible_filesystem? && compatible_kernel
+            command = get_fallocate_command
           else
-            command = get_dd_command(nr)
+            command = get_dd_command
           end
-          Chef::Log.debug("#{nr} swap creation command is '#{command}'")
+          Chef::Log.debug("#{@new_resource} swap creation command is '#{command}'")
           command
         end
 
-        def fallback_swap_creation_command(nr)
-          command = get_dd_command(nr)
-          Chef::Log.debug("#{nr} fallback swap creation command is '#{command}'")
+        def fallback_swap_creation_command
+          command = get_dd_command
+          Chef::Log.debug("#{@new_resource} fallback swap creation command is '#{command}'")
           command
         end
 
@@ -105,38 +105,38 @@ class Chef
           1048576
         end
 
-        def get_fallocate_size(nr)
-          size = block_size * nr.size
-          Chef::Log.debug("#{nr} fallocate size is #{size}")
+        def get_fallocate_size
+          size = block_size * @new_resource.size
+          Chef::Log.debug("#{@new_resource} fallocate size is #{size}")
           size
         end
 
-        def get_fallocate_command(nr)
-          size = get_fallocate_size(nr)
-          command = "fallocate -l #{size} #{nr.path}"
-          Chef::Log.debug("#{nr} fallocate command is '#{command}'")
+        def get_fallocate_command
+          size = get_fallocate_size
+          command = "fallocate -l #{size} #{@new_resource.path}"
+          Chef::Log.debug("#{@new_resource} fallocate command is '#{command}'")
           command
         end
 
-        def get_dd_command(nr)
-          command = "dd if=/dev/zero of=#{nr.path} bs=#{block_size} count=#{nr.size}"
-          Chef::Log.debug("#{nr} dd command is '#{command}'")
+        def get_dd_command
+          command = "dd if=/dev/zero of=#{@new_resource.path} bs=#{block_size} count=#{@new_resource.size}"
+          Chef::Log.debug("#{@new_resource} dd command is '#{command}'")
           command
         end
 
-        def compatible_kernel(nr)
+        def compatible_kernel
           fallocate_location = %x[which fallocate]
-          Chef::Log.debug("#{nr} fallocate location is '#{fallocate_location}'")
+          Chef::Log.debug("#{@new_resource} fallocate location is '#{fallocate_location}'")
           ::File.exists?(fallocate_location.chomp)
         end
 
-        def compatible_filesystem?(nr)
+        def compatible_filesystem?
           compatible_filesystems = ['xfs', 'ext4']
-          parent_directory = ::File.dirname(nr.path)
+          parent_directory = ::File.dirname(@new_resource.path)
           # Get FS info, get second line as first is column headings
           command = "df -T #{parent_directory} | awk 'NR==2 {print $2}'"
           result = %x[#{command}]
-          Chef::Log.debug("#{nr} filesystem listing is '#{result}'")
+          Chef::Log.debug("#{@new_resource} filesystem listing is '#{result}'")
           compatible_filesystems.any? { |fs| result.include? fs }
         end
 
