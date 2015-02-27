1053502cf25bb51d9867cc74988d8b881ae4484e
clongbupt <clongbupt@gmail.com>
2015-02-27 12:02:09 +0900

    update to keep idempotent

--- libraries/swapfile_provider.rb
+++ libraries/swapfile_provider.rb
@@ -183,21 +183,13 @@ class Chef
       # Link: https://help.ubuntu.com/community/SwapFaq#What_is_swappiness_and_how_do_I_change_it.3F
       # Link: https://access.redhat.com/documentation/en-US/Red_Hat_Enterprise_Linux/6/html/Performance_Tuning_Guide/s-memory-tunables.html
       def swappiness
-        proc_command = 'echo 10 > /proc/sys/vm/swappiness'
         Chef::Log.info("#{@new_resource} is setting swappiness value in '/proc/sys/vm/swappiness'")
-        shell_out!(proc_command)
+        ::File.write('/proc/sys/vm/swappiness', '10')
 
-        sysctl = '/etc/sysctl.conf'
-        contents = ::File.readlines(sysctl)
-        addition = 'vm.swappiness = 10'
+        sysctl_command = '/sbin/sysctl vm.swappiness=10'
+        Chef::Log.info("#{@new_resource} is setting swappiness value using sysctl.")
+        shell_out!(sysctl_command)
 
-        if contents.any? { |line| line.strip == addition }
-          Chef::Log.debug("#{@new_resource} already added to /etc/sysctl.conf - skipping")
-        else
-          Chef::Log.info("#{@new_resource} adding entry to #{sysctl} for #{@new_resource.path}")
-
-          contents << "#{addition}\n"
-          ::File.open(sysctl, 'w') { |f| f.write(contents.join('')) }
         end
       end
     end
