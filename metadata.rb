3d8fde66440c3d6cb3c5cee034a6fa07bab073d3
Tim Smith <tsmith@chef.io>
2016-08-05 14:22:12 -0700

    Add new issues_url, source_url and chef_version metadata
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -6,3 +6,6 @@ description 'Manage swap and swapfiles with Chef'
 long_description 'Provides an LWRP for easily creating a managing swap files ' \
                  'and swap partitions in Chef recipes.'
 version '0.3.8'
+source_url 'https://github.com/sethvargo-cookbooks/swap' if respond_to?(:source_url)
+issues_url 'https://github.com/sethvargo-cookbooks/swap/issues' if respond_to?(:issues_url)
+chef_version '>= 11' if respond_to?(:chef_version)
