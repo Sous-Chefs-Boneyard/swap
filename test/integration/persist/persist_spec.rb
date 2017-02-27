c6dd404a8a28e15887f51fa1a9daf1f01b02255c
Richard Shade <rshade@rightscale.com>
2017-02-27 11:29:36 -0600

    adding tr-d to test

--- test/integration/persist/persist_spec.rb
+++ test/integration/persist/persist_spec.rb
@@ -1,3 +1,3 @@
-describe bash('grep /mnt/swap /etc/fstab | awk \'{ print $1 }\'') do
-  its('stdout') { should match %r{\/mnt\/swap/} }
+describe bash('grep /mnt/swap /etc/fstab | awk \'{ print $1 }\' | tr -d \'\n\'') do
+  its('stdout') { should match %r{/mnt/swap} }
 end
